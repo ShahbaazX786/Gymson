a1a24bd9a8a83d2df929d87a8708faed3333ba21
Shahbaaz <shahbaazplatinum@gmail.com>
2022-03-24 13:12:13 +0530

    added routes file and used in index.js

--- Backend/index.js
+++ Backend/index.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 
 const mongoose = require('./db.js');
+const routes = require('./routes/routes.js');
 
 const app = express();
 
@@ -12,3 +13,5 @@ app.use(cors({origin:'http://localhost:4200'}));
 app.listen(3000, ()=>{
     console.log("Server started and running at port 3000!!");
 })
+
+app.use('/users', routes);
