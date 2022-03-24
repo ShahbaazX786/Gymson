521bb9722f371de6d7c25dbd83ba2bae5a6cfb18
Shahbaaz <shahbaazplatinum@gmail.com>
2022-03-24 13:01:04 +0530

    added express (index.js) server file.

---
+++ Backend/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const mongoose = require('./db.js');
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(cors({origin:'http://localhost:4200'}));
+
+app.listen(3000, ()=>{
+    console.log("Server started and running at port 3000!!");
+})
