2ff08cc155a5608e699bb70ec03474d8adff557e
Shahbaaz <shahbaazplatinum@gmail.com>
2022-03-24 13:40:34 +0530

    Added code for --POST API--  and tested in postman

--- Backend/routes/routes.js
+++ Backend/routes/routes.js
@@ -3,4 +3,27 @@ const router = express.Router();
 
 const User = require('../models/user.js');
 
+//API section 
+//basepath== 'http://localhost:3000/users;
+
+//Post API --start--
+router.post('/',(req,res)=>{
+    let user = new User({
+        name : req.body.name,
+        age : req.body.age,
+        city : req.body.city
+    });
+
+    user.save((err,doc)=>{
+        if(!err){
+            res.send(doc);
+        }
+        else{
+            console.log('Error occured in posting data:' + err);
+        }
+    })
+});
+//Post API --end--
+
+
 module.exports = router;
