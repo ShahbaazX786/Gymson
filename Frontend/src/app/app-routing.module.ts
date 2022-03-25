ce20fe068da132a5c2a543e689c8b4fd42ccd02f
Shahbaaz <shahbaazplatinum@gmail.com>
2022-03-25 13:41:59 +0530

    added some components and navbar, footer

--- Frontend/src/app/app-routing.module.ts
+++ Frontend/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
+import { HomeComponent } from './Components/home/home.component';
+import { PricingComponent } from './Components/pricing/pricing.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',component:HomeComponent},
+  {path:'pricing',component:PricingComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './Components/navbar/navbar.component';
+import { FooterComponent } from './Components/footer/footer.component';
+import { RegisterComponent } from './Components/register/register.component';
+import { PricingComponent } from './Components/pricing/pricing.component';
+import { HomeComponent } from './Components/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    RegisterComponent,
+    PricingComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
